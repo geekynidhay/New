25932907f38d371b118e1982152d722d751bc296
Nidhay Rathore <86305198+geekynidhay@users.noreply.github.com>
2024-10-23 21:18:20 +0530

    Update gcd.java

--- gcd.java
+++ gcd.java
@@ -1,31 +1,29 @@
+package test;
 import java.util.Scanner;
 
+public class Main {
+	public static void main (String[] args){
+	    Scanner scanner = new Scanner (System.in);
+	    
+	    System.out.println ("Enter Number 1");
+	    
+	    int num1 = scanner.nextInt();
+	    
+	    System.out.println ("Enter Number 2");
+	     int num2 = scanner.nextInt();
+	     
+	     int ans = GCD(num1, num2);
+	     
+	     System.out.println ("GCD Num 1 : " + num1 + " GCD Num 2 : " + num2 + " Answer is " + ans );
+	}
 
-public class GCD {
-    public static int findingGCD(int a, int b){
-        int ans = 0;
-        for (int i = 1; i <=a; i++){
-            if (a%i==0 && b%i == 0){
+	private static int GCD(int num1, int num2) {
+		int ans = 0;
+        for (int i = 1; i <=num1; i++){
+            if (num1%i==0 && num2%i == 0){
                 ans = i;
             }
         }
-        }
-        return a;
-    }
-
-
-public static void main (String[] args){
-    Scanner scanner = new Scanner (System.in);
-    
-    System.out.println ("Enter Number 1");
-    
-    int num1 = scanner.nextInt();
-    
-    System.out.println ("Enter Number 2");
-     int num2 = scanner.nextInt();
-     
-     int ans = findingGCD(num1, num2);
-     
-     System.out.println ("GCD Num 1 : " + num1 + " GCD Num 2 : " + num2 + " Answer is " + ans );
-}
+        return ans;
+	}
 }
